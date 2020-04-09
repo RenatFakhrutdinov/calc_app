e84f8e7e009d66b3ef61213111b2e1041a4763b3
Renat <rfakhrutdinov1988@gmail.com>
2020-04-09 12:58:11 +0300

    add points array to edge_finder_test

--- test/edge_finder_test.dart
+++ test/edge_finder_test.dart
@@ -6,37 +6,16 @@ import 'package:flutter_test/flutter_test.dart';
 void main() {
   test("EdgeFinder methods return correct values", () {
     final edgeHelper = EdgeFinder();
-    expect(
-        edgeHelper.minX([
-          Point(0.0, 0.0),
-          Point(-1.0, -1.0),
-          Point(1.0, 1.0),
-          Point(-2.0, 2.0)
-        ]),
-        -2.0);
-    expect(
-        edgeHelper.maxX([
-          Point(0.0, 0.0),
-          Point(-1.0, -1.0),
-          Point(1.0, 1.0),
-          Point(-2.0, 2.0)
-        ]),
-        1.0);
-    expect(
-        edgeHelper.minY([
-          Point(0.0, 0.0),
-          Point(-1.0, -1.0),
-          Point(1.0, 1.0),
-          Point(-2.0, 2.0)
-        ]),
-        -1.0);
-    expect(
-        edgeHelper.maxY([
-          Point(0.0, 0.0),
-          Point(-1.0, -1.0),
-          Point(1.0, 1.0),
-          Point(-2.0, 2.0)
-        ]),
-        2.0);
+    List<Point<num>> points = [
+      Point(0.0, 0.0),
+      Point(-1.0, -1.0),
+      Point(1.0, 1.0),
+      Point(-2.0, 2.0)
+    ];
+
+    expect(edgeHelper.minX(points), -2.0);
+    expect(edgeHelper.maxX(points), 1.0);
+    expect(edgeHelper.minY(points), -1.0);
+    expect(edgeHelper.maxY(points), 2.0);
   });
 }
