05d2895f4f818bc56d2abe08d36f3022fdfd4ae7
Renat <rfakhrutdinov1988@gmail.com>
2020-04-10 14:04:49 +0300

    fix: error of getting image url

--- lib/helper/get_plot_image_url.dart
+++ lib/helper/get_plot_image_url.dart
@@ -5,7 +5,9 @@ String getPlotImageUrl(CommonResponseModel model) {
   String url;
   Pods podsWithPlot;
   try {
-    model.queryResult.pods.singleWhere((pods) => pods.title == "Plot");
+    podsWithPlot =
+        model.queryResult.pods.singleWhere((pods) => pods.title == "Plot");
+    url = podsWithPlot.subpods.first.img.src;
   } catch (e) {
     url = 'getPlotImageUrl error: $e';
     print(url);
@@ -13,6 +15,5 @@ String getPlotImageUrl(CommonResponseModel model) {
     ///pict just for fun =)
     return 'https://it.fffuuu.net/Upload/Images/porajeny_vashej_neudachej__sudar.png';
   }
-  url = podsWithPlot.subpods.first.img.src;
   return url;
 }
