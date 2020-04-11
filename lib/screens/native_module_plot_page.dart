27752aa5ff3d3e11f9e0e750c57aa6a4361b5dac
Renat <rfakhrutdinov1988@gmail.com>
2020-04-11 14:50:32 +0300

    add math expression and range texts

--- lib/screens/native_module_plot_page.dart
+++ lib/screens/native_module_plot_page.dart
@@ -1,4 +1,9 @@
+import 'dart:math';
+
+import 'package:calcapp/point_generator/generate_list_of_points.dart';
+import 'package:calcapp/res/numbers.dart';
 import 'package:calcapp/res/strings.dart';
+import 'package:calcapp/widgets/graph_widget.dart';
 import 'package:flutter/material.dart';
 
 class NativeModulePlotPage extends StatefulWidget {
@@ -14,6 +19,15 @@ class NativeModulePlotPage extends StatefulWidget {
 }
 
 class _NativeModulePlotPageState extends State<NativeModulePlotPage> {
+  List<Point> points = [];
+
+  @override
+  void initState() {
+    super.initState();
+    points =
+        generateListOfPoints(widget.mathExpression, widget.from, widget.to);
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -21,8 +35,16 @@ class _NativeModulePlotPageState extends State<NativeModulePlotPage> {
         title: Text(Strings.nativeModule),
         centerTitle: true,
       ),
-      body: Center(
-        child: Text('test'),
+      body: Padding(
+        padding: const EdgeInsets.all(standardPadding),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceAround,
+          children: <Widget>[
+            Text(widget.mathExpression),
+            GraphWidget(points: points),
+            Text('x from ${widget.from} to ${widget.to}')
+          ],
+        ),
       ),
     );
   }
