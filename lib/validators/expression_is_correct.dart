8107ce83e10557779fced2e29085ef134653a7f0
Renat <rfakhrutdinov1988@gmail.com>
2020-04-11 17:40:53 +0300

    create fromIsNotEqualTo function

---
+++ lib/validators/expression_is_correct.dart
@@ -0,0 +1,21 @@
+//  String test() {
+//    ///if sqrt && (from<0 \\ to<0) { error }
+//    ///if !contain x {error}
+//    ///if /x && from<0 && to >0 {do something}
+//    ///check from!=to
+//
+//    String ttt;
+//    try {
+//      FunctionOfX f = FunctionOfX(fromExpression: 'sqrt(x)');
+//      ttt = f(-1).toString();
+//    } catch (e) {
+//      return e.toString();
+//    }
+//
+//    if (ttt == "NaN") {
+//      return "нанананананан";
+//    } else if (ttt == "Infinity") {
+//      return 'na nol delit nelsya';
+//    } else
+//      return ttt;
+//  }
---
+++ lib/validators/from_is_not_equal_to.dart
@@ -0,0 +1,6 @@
+bool fromIsNotEqualTo(num from, num to) {
+  if (from == to) {
+    return false;
+  } else
+    return true;
+}
