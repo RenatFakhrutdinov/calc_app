976d951a999d7998211d280df439bff49f0e5620
Renat <rfakhrutdinov1988@gmail.com>
2020-04-11 15:11:57 +0300

    correct suggestions of expressions

--- lib/res/strings.dart
+++ lib/res/strings.dart
@@ -1,10 +1,10 @@
 class Strings {
   static const String title = "CalcApp";
-  static const String exp1 = "2x + 6";
+  static const String exp1 = "2 * x + 6";
   static const String exp2 = "10 - x";
   static const String exp3 = "x/2";
   static const String exp4 = "x * x";
-  static const String exp5 = "-2x";
+  static const String exp5 = "-2 * x";
   static const String plotTheFunction = "Plot the function with";
   static const String nativeModule = "Native module";
   static const String wolframAlphaApi = "Wolfram Alpha API";
