d080e63649f7483a16f38f9eea83bcb1580342cd
Renat <rfakhrutdinov1988@gmail.com>
2020-05-06 20:46:35 +0300

    add dio lib

--- lib/api/wolfram_alpha_api.dart
+++ lib/api/wolfram_alpha_api.dart
@@ -1,29 +1,31 @@
 import 'dart:convert';
 
 import 'package:calcapp/model/common_response_model.dart';
+import 'package:dio/dio.dart';
 import 'package:flutter/foundation.dart';
-import 'package:http/http.dart' as http;
 
 class WolframAlphaApi {
   static const String wolframAppId = "5GJP3X-4378GYWVUL";
-  static const String wolframAlphaApiUrl =
-      "https://api.wolframalpha.com/v2/query?input=plot";
 
   Future<CommonResponseModel> getPlot(
       {@required String mathExpression,
       @required num from,
       @required num to}) async {
-    final plotUrl =
-        "$wolframAlphaApiUrl $mathExpression, x=$from..$to&format=image&output=JSON&appid=$wolframAppId";
-    http.Response response;
+    Response response;
     try {
-      response = await http.get(plotUrl);
+      response = await Dio()
+          .get("https://api.wolframalpha.com/v2/query", queryParameters: {
+        "input": "plot $mathExpression, x=$from..$to",
+        "format": "image",
+        "output": "JSON",
+        "appid": wolframAppId
+      });
     } catch (e) {
       throw ('getPlot error: $e');
     }
 
     CommonResponseModel model;
-    model = CommonResponseModel.fromJson(jsonDecode(response.body));
+    model = CommonResponseModel.fromJson(jsonDecode(response.data));
     return model;
   }
 }
